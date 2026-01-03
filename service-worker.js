ac36852fe7252657150198880e2182bbb56bca3c
Maxxxter86 <max_tito86@hotmail.com>
2026-01-03 11:18:19 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "mus-marcador-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./marcador-mus.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
